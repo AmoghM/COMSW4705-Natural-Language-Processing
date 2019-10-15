5e8ad6c4051a1c2f0836923ebe97b38a167184dd
Amogh Mishra <amoghmishra23@gmail.com>
2019-10-15 18:02:47 -0400

    Added first iteration of the CKY and Grammar assignment. Contains bugs

--- hw2/cky.py
+++ hw2/cky.py
@@ -92,39 +92,146 @@ class CkyParser(object):
 
     def is_in_language(self,tokens):
         """
-        Membership checking. Parse the input tokens and return True if 
+        Membership checking. Parse the input tokens and return True if
         the sentence is in the language described by the grammar. Otherwise
         return False
         """
         # TODO, part 2
-        return False 
+        n = len(tokens)
+        prob_table = {}
+        parse_table = {}
+        table = [[[] for i in range(0, n)] for j in range(0, n)]
+        # print(len(table[0]))
+
+        for i in range(0,len(tokens)):
+            rules = grammar.rhs_to_rules[(tokens[i],)]
+            for rule in rules:
+                table[i][i].append(rule[0])
+                # parse_table[(i,i+1)] = {rule[0]:(i,i+1)}
+                # prob_table[(i,i+1)] = {rule[0]:rule[2]}
+
+        # print(table)
+        # exit()
+        for length in range(2,n+1):
+            for i in range(0,n-length+1):
+                j = i+length
+                for k in range(i+1,j):
+                    # print(n,length,i,k,j)
+                    # print("TABLE[I][K]", i, k-1, table[i][k-1])
+                    # print("TABLE[K][J]", k, j-1, table[k][j-1])
+                    if len(table[i][k-1]) is not 0 and len(table[k][j-1]) is not 0:
+                        import itertools
+                        union = list(itertools.product(table[i][k-1],table[k][j-1]))
+                        # print("UNION IS: ", union)
+                        for un in union:
+                            if un in grammar.rhs_to_rules:
+                                non_terminal = grammar.rhs_to_rules[un]
+                                for nt in non_terminal:
+                                    # if (i,j) not in prob_table:
+                                    #     prob_table[(i,j)] = {}
+                                    #     parse_table[(i, j)] = {}
+
+                                    table[i][j-1].append(nt[0])
+                                    # print(prob_table)
+                                    # print(i,k,j)
+                                    # input()
+
+                                    # try:
+                                    #     prob = prob_table[(i,j)][nt[0]]
+                                    #     if prob > nt[2]:
+                                    #         raise KeyError
+                                    #         # prob_table[(i,j)][nt[0]] = max(prob, nt[2])
+                                    # except KeyError:
+                                    #     prob_table[(i,j)][nt[0]] = nt[2]
+                                    #     left_key = list(parse_table[(i,k)].keys())[0]
+                                    #     left_child = (left_key,i,k)
+                                    #     right_key = list(parse_table[(k,j)].keys())[0]
+                                    #     right_child = (right_key,k,j)
+                                    #     parse_table[(i,j)][nt[0]] = (left_child, right_child)
+        # print("NOW THE TABLE IS")
+        # print(table[0])
+        # print(parse_table[(0,3)])
+        if 'TOP' in table[0][n-1]:
+            return True
+
+        return False
        
     def parse_with_backpointers(self, tokens):
         """
         Parse the input tokens and return a parse table and a probability table.
         """
         # TODO, part 3
-        table= None
-        probs = None
+        table= {}
+        probs = {}
+        n = len(tokens)
+
+        for tok in range(0,n):
+            span = (tok, tok+1)
+            if span not in table:
+                table[span] = {}
+                probs[span] = {}
+
+            non_terminals = grammar.rhs_to_rules[(tokens[tok],)]
+            for nt in non_terminals:
+                try:
+                    if math.log2(nt[2]) > probs[span][nt[0]]:
+                        raise KeyError
+                        # probs[span][nt[0]] = math.log(nt[2])
+                        # table[span][nt[0]] = tokens[tok]
+                except KeyError:
+                    table[span][nt[0]] = tokens[tok]
+                    probs[span][nt[0]] = math.log2(nt[2])
+
+        for length in range(2, n + 1):
+            for i in range(0, n - length + 1):
+                j = i + length
+                for k in range(i + 1, j):
+                      try:
+                          import itertools
+                          union = list(itertools.product(table[(i,k)].keys(), table[(k,j-1)].keys()))
+                          for un in union:
+                              if un in grammar.rhs_to_rules:
+                                  non_terminal = grammar.rhs_to_rules[un]
+                                  for nt in non_terminal:
+                                      if nt[0] not in table[(i,j)]:
+                                          table[(i, j)][nt[0]] = ((un[0],i,k),(un[1],k,j))
+                                          probs[(i,j)][nt[0]] = math.log2(nt[2]) + probs[(i,k)][un[0]] + probs[(k,j)][un[1]]
+                                      else:
+                                          if probs[(i,j)][nt[0]] < math.log2(un[2]) + probs[(i,k)][un[0]] + probs[(k,j)][un[1]]:
+                                              table[(i, j)][nt[0]] = ((un[0], i, k), (un[1], k + 1, j + 1))
+                                              probs[(i, j)][nt[1]] = math.log2(nt[2]) + probs[(i, k + 1)][un[0]] + probs[(k + 1, j + 1)][un[1]]
+
+                      except KeyError:
+                          continue
         return table, probs
 
 
-def get_tree(chart, i,j,nt): 
+def get_tree(chart,i,j,nt):
     """
     Return the parse-tree rooted in non-terminal nt and covering span i,j.
     """
     # TODO: Part 4
-    return None 
- 
+
+    if j-i == 1:
+        output = (nt, chart[i, j][nt])
+        return output
+
+    out1 = get_tree(chart, chart[(i,j)][nt][0][1], chart[(i,j)][nt][0][2], chart[(i,j)][nt][0][0])
+    out2 = get_tree(chart, chart[(i,j)][nt][1][1], chart[(i,j)][nt][1][2], chart[(i,j)][nt][1][0])
+    return (nt, out1, out2)
+
        
 if __name__ == "__main__":
     
     with open('atis3.pcfg','r') as grammar_file: 
         grammar = Pcfg(grammar_file) 
         parser = CkyParser(grammar)
-        toks =['flights', 'from','miami', 'to', 'cleveland','.'] 
-        #print(parser.is_in_language(toks))
-        #table,probs = parser.parse_with_backpointers(toks)
-        #assert check_table_format(chart)
-        #assert check_probs_format(probs)
-        
+        toks =['flights', 'from','miami', 'to', 'cleveland','.']
+        # toks = ['miami', 'flights', 'cleveland', 'from', 'to', '.']
+        print(parser.is_in_language(toks))
+        table, probs = parser.parse_with_backpointers(toks)
+        assert check_table_format(table)
+        assert check_probs_format(probs)
+        print(table)
+        print(probs)
+        get_tree(table, 0, len(toks), grammar.startsymbol)
--- hw2/grammar.py
+++ hw2/grammar.py
@@ -20,7 +20,6 @@ class Pcfg(object):
         self.read_rules(grammar_file)      
  
     def read_rules(self,grammar_file):
-        
         for line in grammar_file: 
             line = line.strip()
             if line and not line.startswith("#"):
@@ -42,16 +41,37 @@ class Pcfg(object):
         rhs = tuple(rhs_s.strip().split())
         return (lhs, rhs, prob)
 
-    def verify_grammar(self):
+    def verify_grammar(self,key):
         """
         Return True if the grammar is a valid PCFG in CNF.
         Otherwise return False. 
         """
         # TODO, Part 1
+        try:
+            lhs_rule = self.lhs_to_rules[key]
+            sum_prob = []
+            for lhs in lhs_rule:
+                sum_prob.append(lhs[2])
+                print("LHS IS: ", lhs)
+                rhs = lhs[1]
+                if rhs[0].isupper() and rhs[1].isupper():
+                    pass
+                elif rhs[0].islower() and len(rhs)==1:
+                    pass
+                else:
+                    return False
+        except KeyError:
+            return False
+        if round(fsum(sum_prob),2) == 1.0:
+            print(sum_prob)
+            return True
+
         return False 
 
 
 if __name__ == "__main__":
     with open(sys.argv[1],'r') as grammar_file:
         grammar = Pcfg(grammar_file)
-        
+        # print(grammar.lhs_to_rules['FLIGHTS'])
+        # print(grammar.verify_grammar('FLIGHTS'))
+        # print(grammar.rhs_to_rules[('ABOUT','NP')])
